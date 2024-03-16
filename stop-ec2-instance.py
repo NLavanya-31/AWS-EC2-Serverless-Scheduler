618bfcee47c4a346b0598003b87c780169e4caac
N Lavanya <155809688+NLavanya-31@users.noreply.github.com>
2024-03-16 15:11:09 +0530

    Update stop-ec2-instance.py

--- stop-ec2-instance.py
+++ stop-ec2-instance.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     ec2_client = boto3.client('ec2', region_name='ap-south-1') # your Region name
 
     # Specify the instance ID of the EC2 instance you want to stop
-    instance_id = 'i-051262059d6f6350a' # your Instance ID
+    instance_id = 'i-0fd1720a2634065c6' # your Instance ID
 
     # Stop the EC2 instance
     try:
